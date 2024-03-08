56f2ec55c49db2d35e794db7e26343aa3c74996e
tboulund <tbmh@easv.dk>
2024-03-08 15:07:39 +0100

    Delivery pipeline

--- test/e2e/count-forecasts.js
+++ test/e2e/count-forecasts.js
@@ -6,17 +6,11 @@ import { Selector } from 'testcafe';
 import { ClientFunction } from 'testcafe';
 
 fixture `Count Forecasts`
-    .page `http://localhost:5001/`;
+    .page `http://84.247.167.129:5001/`;
 
 test('Count Forecasts', async t => {
     // Connects to the API and gets the count of forecasts
-    const getCount = ClientFunction(() => {
-        return fetch("http://localhost:5002/weatherforecast")
-            .then(response => response.json())
-            .then(data => data.length);
-    });
-
-    let forecastCount = (await t.request("http://localhost:5002/weatherforecast")).body.length;
+    let forecastCount = (await t.request("/weatherforecast")).body.length;
 
     // Counts the number of table rows in the forecast table.
     let tableRowCount = Selector("table#forecasts tbody tr").count;
